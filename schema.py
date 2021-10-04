4cd1ad2e371d737b2dc9253b2abc682b1d8fb3b8
Simrun Heir <sunsimheir@gmail.com>
2021-10-04 16:22:29 -0400

    fixed local schema.py

--- schema.py
+++ schema.py
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-# MySQL schema
-
-ID_DATA_TYPE = "varchar(200)"
-
-
-TABLES['Individual'] = f"""CREATE TABLE IF NOT EXISTS `gtfs` (
-    `feed_id` {ID_DATA_TYPE} NOT NULL,
-    `gtfs_timestamp` datetime NOT NULL UNIQUE,
-    `file` longblob NOT NULL,
-    `gtfs_url` text NOT NULL,
-    `parsed` boolean NOT NULL DEFAULT 0,
-    PRIMARY KEY (`feed_id`, `gtfs_timestamp`),
-    FOREIGN KEY (`feed_id`) REFERENCES feeds (`feed_id`)
-    ) ENGINE=InnoDB
-    """
-=======
 # MySQL schema
 
 ID_DATA_TYPE = "varchar(200)"
@@ -64,4 +47,3 @@ COLUMNS['marriages'] = ("mid", "marrydate", "divorced", "divorcedate",
 CONSTRAINTS.append(
     f"ALTER TABLE individuals ADD CONSTRAINT `fk_parentmarriage` FOREIGN KEY (`parentmarriage`) REFERENCES `marriages` (`mid`)"
 )
->>>>>>> 7813fd925b688e968fde90b4c40a1265c4af1c01
